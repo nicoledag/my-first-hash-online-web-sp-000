62364865ffd11a1c7e8e4e5e15a815f8f5b0a49d
Nicole D <nnardella@gmail.com>
2018-11-01 13:44:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,7 +36,7 @@ def adding
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 
-puts shipping_manifest
+shipping_manifest
 
   # add 4 gun powder to the shipping_manifest hash below
 
